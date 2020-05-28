c6b124e8955c881482500cdbb645aa700af3c173
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-05-28 16:41:08 -0400

    started async function

--- Develop/app.js
+++ Develop/app.js
@@ -15,7 +15,7 @@ const render = require("./lib/htmlRenderer");
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // Questions stored in object array
-const prompts = [{
+const prompt = [{
     name: "name",
     type: "input",
     message: "What is the employee's name?"
@@ -44,6 +44,11 @@ const prompts = [{
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+async function prompt(){
+    const answer = "";
+    
+}
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
