a5bed0da4fc1696fa4a321bbd237d08d699b382b
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-05-29 18:18:25 -0400

    added constructor classes

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,5 +1,7 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 class Intern extends Employee {
-
+    constructor(){
+        
+    }
 }
 module.exports = Intern;
