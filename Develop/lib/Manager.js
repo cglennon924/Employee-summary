14af142703cf4d70e6937510609aeead545f1e38
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-05-29 22:41:28 -0400

    fix require in js files

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,4 +1,4 @@
-require ("./Employee")
+const Employee = require ("./Employee")
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 class Manager extends Employee {
     constructor(name, id, email, officeNumber){
