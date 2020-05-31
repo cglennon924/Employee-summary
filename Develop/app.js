6e9cdd1a8ea4e7d086183119ced882bfaf1b5111
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-05-30 23:36:53 -0400

    updated readme and app.js

--- Develop/app.js
+++ Develop/app.js
@@ -11,8 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Variable storing inquirer prompt
 let employeeEl = []
 // Questions stored in object array
 const sheet = [{
